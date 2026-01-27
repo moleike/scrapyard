93b7a0599184411592a9e3ab3637046f206983c8
Alexandre Moreno <alexmorenocano@gmail.com>
2026-01-27 22:43:06 +0100

    store log pointers in the index

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@
 //! a simple in-memory key/value store that maps strings to strings
 
 use serde::{Deserialize, Serialize};
+use serde_jsonlines::JsonLinesReader;
 use std::fs::{self, read_to_string, File, OpenOptions};
-use std::io::{self, prelude::*};
+use std::io::{self, prelude::*, BufReader};
+use std::os::unix::fs::MetadataExt;
 use std::{collections::HashMap, path::PathBuf, result};
 use thiserror::Error;
 
@@ -22,8 +24,12 @@ use thiserror::Error;
 /// assert_eq!(kvs.get("foo".into()), Some("bar".into()));
 /// ```
 pub struct KvStore {
-    cache: HashMap<String, String>,
+    keydir: HashMap<String, ValueInfo>,
     wal_path: PathBuf,
+}
+
+struct ValueInfo {
+    wal_offset: u64,
 }
 
 #[derive(Error, Debug)]
@@ -42,8 +48,8 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
-    Set(String, String),
-    Del(String),
+    Set(String, String, u64),
+    Del(String, u64),
 }
 
 impl KvStore {
@@ -57,40 +63,62 @@ impl KvStore {
             .write(true)
             .open(path.clone())?;
 
-        let cache: HashMap<String, String> = HashMap::new();
+        let mut keydir: HashMap<String, ValueInfo> = HashMap::new();
+
+        let metadata = fs::metadata(&path)?;
+
+        if metadata.len() > 0 {
+            let iter = serde_jsonlines::json_lines::<Command, _>(&path)?;
+
+            let cmds = iter.collect::<io::Result<Vec<Command>>>()?;
+
+            for cmd in cmds {
+                match cmd {
+                    Command::Set(k, _, offset) => {
+                        keydir.insert(k, ValueInfo { wal_offset: offset })
+                    }
+                    Command::Del(k, _) => keydir.remove(&k),
+                };
+            }
+        }
 
         Ok(KvStore {
-            cache,
+            keydir,
             wal_path: path,
         })
     }
 
     /// get `key` if it exists
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        let path = &self.wal_path;
-        let metadata = fs::metadata(path)?;
+        if let Some(value_info) = self.keydir.get(&key) {
+            let path = &self.wal_path;
+            let mut fp = BufReader::new(File::open(path)?);
 
-        if metadata.len() > 0 {
-            let cmds =
-                serde_jsonlines::json_lines(path)?.collect::<std::io::Result<Vec<Command>>>()?;
+            fp.seek_relative(value_info.wal_offset as i64)?;
 
-            for cmd in cmds {
-                match cmd {
-                    Command::Set(k, v) => self.cache.insert(k, v),
-                    Command::Del(k) => self.cache.remove(&k),
-                };
-            }
+            let mut reader = JsonLinesReader::new(fp);
+
+            Ok(reader.read::<Command>()?.map(|cmd| match cmd {
+                Command::Set(_, value, _) => value,
+                Command::Del(_, _) => panic!(),
+            }))
+        } else {
+            Ok(None)
         }
-
-        Ok(self.cache.get(&key).cloned())
     }
 
     /// set or replace `key` to `value`
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        serde_jsonlines::append_json_lines(
-            self.wal_path.clone(),
-            &[Command::Set(key, value)],
-        )?;
+        let path = self.wal_path.clone();
+
+        let metadata = fs::metadata(&path)?;
+
+        let offset = metadata.size();
+
+        serde_jsonlines::append_json_lines(path, &[Command::Set(key.clone(), value, offset)])?;
+
+        self.keydir
+            .insert(key.clone(), ValueInfo { wal_offset: offset });
 
         Ok(())
     }
@@ -98,10 +126,18 @@ impl KvStore {
     /// remove an key if exists and return the value
     pub fn remove(&mut self, key: String) -> Result<()> {
         if let Some(_) = self.get(key.clone())? {
+            let path = self.wal_path.clone();
+
+            let metadata = fs::metadata(&path)?;
+
+            let offset = metadata.size();
+
             serde_jsonlines::append_json_lines(
                 self.wal_path.clone(),
-                &[Command::Del(key.clone())],
+                &[Command::Del(key.clone(), offset)],
             )?;
+
+            self.keydir.remove(&key);
 
             Ok(())
         } else {
